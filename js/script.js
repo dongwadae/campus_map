49a995a14f5dc477d3ae445ad357e37c83371d3d
Lee HoJoon <leehojoonlee0820@gmail.com>
2024-03-17 00:15:52 +0900

    Revert "asdsadsa"
    This reverts commit e0fdd66d444aaf485ee90c9ef0a52c854eb18a16.

--- js/script.js
+++ js/script.js
@@ -425,7 +425,7 @@ function displayOVerlay(data) {
     "," +
     data.longitude +
     ');">' +
-    '       <span class="locpin">' +
+    '       <span class="num">' +
     data.ord +
     "</span>" +
     "   </a>" +
